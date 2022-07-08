c814e8bc150b5b5aa582d932a17da8598acf5d06
Patrick Mullen <patrickmullenparamedic@gmil.com>
2022-07-07 21:11:52 -0400

    still going

---
+++ script.js
@@ -0,0 +1,67 @@
+const products = [
+{
+    id: pineapple,
+    name: "Pineapple",
+    price: 2.99,
+    price: 0.99, 
+    imgSrc: "",
+},
+{
+    id: toiletpaper,
+    name: "toiletpaper",
+    price: 10.99,
+    price: 2.00,
+    imgSrc:"",
+},
+{
+    id: lysol,
+    name: "lysol",
+    price: 7.00,
+    price: 3.99,
+    imgSrc: "",
+},
+{
+    id: facemasks,
+    name: "facemasks",
+    price: 2.99,
+    price: 5.99,
+    imgSrc: "",
+},
+{
+    id: steak,
+    name: "steak",
+    price: 8.99,
+    price: 12.99,
+    imgSrc: "",
+    
+},
+{
+    id: apples,
+    name: "apples",
+    price: 3.00,
+    price: 4.99,
+    imgSrc: "",
+},
+{
+    id: whitebread,
+    name: "whitebread",
+    price: 0.99,
+    price: 1.00,
+    imgSrc: "",
+},
+{
+    id: milk,
+    name: "milk",
+    price: 5.00,
+    price: 2.50,
+    imgSrc: "",
+},
+{
+    id: icecream,
+    name: "ice cream",
+    price: 1.50,
+    price: 1.25,
+    imgSrc: "",
+}
+
+];
