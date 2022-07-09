08dbdaf2f2e272fbd50c441a28715111d82c2778
Patrick Mullen <patrickmullenparamedic@gmil.com>
2022-07-09 12:50:45 -0400

    closer

---
+++ app.js
@@ -0,0 +1,12 @@
+//selecting elements
+
+const productsEl = document.querySelector(".products");
+
+function renderProducts(){
+    products.forEach( (product) => {
+        productsEl.innerHTML += `
+        
+        
+        `
+    })
+}
