022113449bac8056b041a4d199dbd3b4d5ce7970
Patrick Mullen <patrickmullenparamedic@gmil.com>
2022-07-16 21:00:04 -0400

    i am happy

---
+++ src/data.js
@@ -0,0 +1,55 @@
+//my item array
+
+
+let shopItemsData = [{
+    id:"hi",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+},
+{   
+    id:"bye",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"},
+
+{   
+    id:"hola",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"},
+
+{
+    id:"adios",
+    name: "Mens Suit",
+    price: 100,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
